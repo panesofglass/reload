575b5ad9f29618dafaa428df37ac76d33a7b2e1a
Ryan Riley <ryan@tachyus.com>
2015-05-19 23:43:25 -0500

    Add AssemblyInfo and Run Targets.

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Demonstrate building and watching F# compilations using only FAKE.
 
 ## Targets
 
-* Compile -> builds a `.exe` from any `.fs` files in the root directory
-* Default -> runs the `Compile` target
-* Watch -> should recompile the `.exe` for any changes to any `.fs` files in the directory
+* AssemblyInfo - generates an `AssemblyInfo.fs`
+* Compile - builds a `.exe` from any `.fs` files in the root directory
+* Default - runs the `Compile` target
+* Run - Runs the generated `.exe`
+* Watch - should recompile the `.exe` for any changes to any `.fs` files in the directory, but currently appears to blocked by `System.Console.ReadLine`
--- build.fsx
+++ build.fsx
@@ -2,26 +2,51 @@
 #r "FakeLib.dll"
 
 open Fake
+open Fake.AssemblyInfoFile
+open Fake.EnvironmentHelper
 open Fake.FscHelper
+open Fake.ProcessHelper
+
+let version = environVarOrDefault "version" "1.0.0"
+let exeName = environVarOrDefault "exeName" "main.exe"
+
+Target "AssemblyInfo" <| fun _ ->
+    CreateFSharpAssemblyInfo "./AssemblyInfo.fs"
+        [Attribute.Title "Reload Demo"
+         Attribute.Description "Demonstration of compiling and watching F# tools with FAKE"
+         Attribute.Version version
+         Attribute.FileVersion version]
 
 Target "Compile" <| fun _ ->
-    ["Main.fs"]
+    !!"*.fs"
+    |> List.ofSeq
     |> Fsc (fun p ->
-            { p with References = ["packages/FSharp.Core/lib/net40/FSharp.Core.dll"] })
+            { p with
+                Output = exeName
+                References = ["packages/FSharp.Core/lib/net40/FSharp.Core.dll"] })
+
+Target "Run" <| fun _ ->
+    Shell.Exec exeName |> ignore
 
 Target "Watch" <| fun _ ->
     use watcher =
         !!"*.fs"
+        --"AssemblyInfo.fs"
         |> WatchChanges (fun changes ->
             tracefn "%A" changes
-            Run "Compile")
+            Run "Run")
 
+    // TODO: This appears to block.
     System.Console.ReadLine() |> ignore
     watcher.Dispose()
 
 Target "Default" DoNothing
 
-"Compile"
+"AssemblyInfo"
+==> "Compile"
 ==> "Default"
+
+"Default"
+==> "Run"
 
 RunTargetOrDefault "Default"
